33a239615cbfdfbaf79ca18f6c4f3b100e883b0d
hexplus <jframonge@gmail.com>
2016-09-08 14:19:23 -0600

    Adding mapping for TypeScript in License Plugin

--- orcid-web/src/main/webapp/static/javascript/angular2/app/dist/angular2Orcid.js
+++ orcid-web/src/main/webapp/static/javascript/angular2/app/dist/angular2Orcid.js
@@ -1,3 +1,35 @@
+/*
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
+/*
+ * =============================================================================
+ *
+ * ORCID (R) Open Source
+ * http://orcid.org
+ *
+ * Copyright (c) 2012-2014 ORCID, Inc.
+ * Licensed under an MIT-Style License (MIT)
+ * http://orcid.org/open-source-license
+ *
+ * This copyright and license information (including a link to the full license)
+ * shall be included in its entirety in all copies or substantial portion of
+ * the software.
+ *
+ * =============================================================================
+ */
 var Greeter = (function () {
     function Greeter(greeting) {
         this.greeting = greeting;
